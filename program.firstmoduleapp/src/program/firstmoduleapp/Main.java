0ea86fcca59b5665c508644fb1aa1ae1ab40f8de
LUNKUS <sigitas@lunkus.com>
2020-05-06 08:55:50 +0300

    module-info.java added

--- program.firstmoduleapp/src/program/firstmoduleapp/Main.java
+++ program.firstmoduleapp/src/program/firstmoduleapp/Main.java
@@ -1,6 +1,7 @@
 package program.firstmoduleapp;
 
 public class Main {
+
     public static void main(String[] args) {
 
     }
--- program.secondmoduledatabase/src/program/secodmoduledatabase/Course.java
+++ program.secondmoduledatabase/src/program/secodmoduledatabase/Course.java
@@ -1,4 +1,23 @@
 package program.secodmoduledatabase;
 
 public class Course {
+
+    private String courseName;
+    private String courseAuthor;
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseAuthor() {
+        return courseAuthor;
+    }
+
+    public void setCourseAuthor(String courseAuthor) {
+        this.courseAuthor = courseAuthor;
+    }
 }
